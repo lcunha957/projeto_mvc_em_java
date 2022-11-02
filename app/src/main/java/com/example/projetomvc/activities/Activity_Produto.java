0e4eaa963651945d7fc63138b461589f88ed11d5
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-01 23:52:10 -0300

    aula 09
    .

--- app/src/main/java/com/example/projetomvc/activities/Activity_Produto.java
+++ app/src/main/java/com/example/projetomvc/activities/Activity_Produto.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.projetomvc.R;
+import com.example.projetomvc.model.Produto;
 
 public class Activity_Produto extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class Activity_Produto extends AppCompatActivity {
      private EditText edtQuantidadeProduto;
      private EditText edtPrecoProduto;
      private Button btnSalvarProduto;
+
+     private Produto produto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,34 @@ public class Activity_Produto extends AppCompatActivity {
         edtPrecoProduto = (EditText) findViewById(R.id.edtPrecoProduto);
         btnSalvarProduto = (Button) findViewById(R.id.btnSalvarProduto);
 
+    }
+
+    private Produto getDadosProdutoDoFormulario() {
+     this.produto = new Produto();
+
+       // se tiver algo no campo nome do formulário, esse algo vai pra coluna Nome da tabela Produto
+        if (this.edtNomeProduto.getText().toString().isEmpty() == false){
+         this.produto.setNome(this.edtNomeProduto.getText().toString());
+        } else{
+            return null;
+        }
+
+        // se tiver algo no campo quantidade do formulário, esse algo vai pra coluna qtde da tabela Produto
+        if(this.edtQuantidadeProduto.getText().toString().isEmpty() == false){
+            int quantidadeproduto = Integer.parseInt(this.edtQuantidadeProduto.getText().toString());
+            this.produto.setQuantidadeEmEstoque(quantidadeproduto);
+        } else{
+            return null;
+        }
+
+        // se tiver algo no campo preço do formulário, esse algo vai pra coluna preço da tabela Produto
+        if(this.edtPrecoProduto.getText().toString().isEmpty() == false){
+            double precoproduto = Double.parseDouble(this.edtPrecoProduto.getText().toString());
+            this.produto.setPreco(precoproduto);
+        } else{
+            return null;
+        }
+
+        return produto;
     }
 }
