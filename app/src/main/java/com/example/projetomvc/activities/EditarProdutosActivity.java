1a9a8e622a5bd6cae23a16e21d639a548d9628ac
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-02 05:48:50 -0300

    aula 24
    .

--- app/src/main/java/com/example/projetomvc/activities/EditarProdutosActivity.java
+++ app/src/main/java/com/example/projetomvc/activities/EditarProdutosActivity.java
@@ -3,14 +3,46 @@ package com.example.projetomvc.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.projetomvc.R;
+import com.example.projetomvc.model.Produto;
 
 public class EditarProdutosActivity extends AppCompatActivity {
 
+    private EditText edtIdProduto;
+    private EditText edtNomeProduto;
+    private EditText edtPrecoProduto;
+    private EditText edtEstoqueProduto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_produtos);
+
+        edtIdProduto = (EditText) findViewById(R.id.edtIdProduto);
+        edtNomeProduto = (EditText) findViewById(R.id.edtNomeProduto);
+        edtPrecoProduto = (EditText) findViewById(R.id.edtPrecoProduto);
+        edtEstoqueProduto = (EditText) findViewById(R.id.edtEstoqueProduto);
+
+        // recuperar os dados do produto
+        Bundle bundleDadosProduto = getIntent().getExtras();
+
+        Produto produto = new Produto();
+        produto.setId(bundleDadosProduto.getLong("id_produto"));
+        produto.setNome(bundleDadosProduto.getString("nome_produto"));
+        produto.setPreco(bundleDadosProduto.getDouble("preco_produto"));
+        produto.setQuantidadeEmEstoque(bundleDadosProduto.getInt("estoque_produto"));
+
+        // coloca os dados na interface do produto
+        this.setDadosProduto(produto);
+    }
+
+    private void setDadosProduto(Produto produto){
+        // passando os dados pro editText para capturar as alterações
+        this.edtIdProduto.setText(String.valueOf(produto.getId()));
+        this.edtNomeProduto.setText(produto.getNome());
+        this.edtPrecoProduto.setText(String.valueOf(produto.getPreco()));
+        this.edtEstoqueProduto.setText(String.valueOf(produto.getQuantidadeEmEstoque()));
+
     }
 }
