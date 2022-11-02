70d0505d269c85d4d2c467c09314b3a4049d7c92
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-01 23:40:09 -0300

    aula 08
    .

--- app/src/main/java/com/example/projetomvc/activities/Activity_Produto.java
+++ app/src/main/java/com/example/projetomvc/activities/Activity_Produto.java
@@ -3,14 +3,28 @@ package com.example.projetomvc.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.projetomvc.R;
 
 public class Activity_Produto extends AppCompatActivity {
 
+     private EditText edtNomeProduto;
+     private EditText edtQuantidadeProduto;
+     private EditText edtPrecoProduto;
+     private Button btnSalvarProduto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto);
+
+       // ligando a activity com o xml....
+        edtNomeProduto = (EditText) findViewById(R.id.edtNomeProduto);
+        edtQuantidadeProduto = (EditText) findViewById(R.id.edtQuantidadeProduto);
+        edtPrecoProduto = (EditText) findViewById(R.id.edtPrecoProduto);
+        btnSalvarProduto = (Button) findViewById(R.id.btnSalvarProduto);
+
     }
 }
