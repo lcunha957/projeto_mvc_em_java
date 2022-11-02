72c6413bec26a543c60a7d79002bf7cb97d116ae
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-02 17:38:08 -0300

    aula 35
    .

--- app/src/main/java/com/example/projetomvc/activities/ActivityVenda.java
+++ app/src/main/java/com/example/projetomvc/activities/ActivityVenda.java
@@ -3,15 +3,18 @@ package com.example.projetomvc.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.projetomvc.R;
 import com.example.projetomvc.controller.ProdutoController;
 import com.example.projetomvc.dbHelper.ConexaoSQLite;
+import com.example.projetomvc.model.ItemDoCarrinho;
 import com.example.projetomvc.model.Produto;
 
 import java.util.List;
@@ -21,6 +24,10 @@ public class ActivityVenda extends AppCompatActivity {
     private Spinner spnProdutos;
     private List<Produto> listaProduto;
     private ProdutoController produtoController;
+    private Button btnAddProduto;
+    private Button btnFinalizarVenda;
+    private EditText quantidadeItem; // pra pegar quantos itens for adicionados ao carrinho
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +48,35 @@ public class ActivityVenda extends AppCompatActivity {
         ArrayAdapter<Produto> spnProdutoAdapter = new ArrayAdapter<Produto>
                 (this, android.R.layout.simple_spinner_item, this.listaProduto);
         // o spnPorudtos vai usar o adapter spnProdutoAdapter. O adapter é quem vai manusear.s
-       this.spnProdutos.setAdapter(spnProdutoAdapter);
+        this.spnProdutos.setAdapter(spnProdutoAdapter);
         //end spinner
+
+        this.btnAddProduto = (Button) findViewById(R.id.btnAddProduto);
+        this.btnFinalizarVenda = (Button) findViewById(R.id.btnFinalizarVenda);
+        this.quantidadeItem = (EditText) findViewById(R.id.edtQuantidadeProduto);
+    }
+
+    // ao clicar no botão, pega os dados do produto e coloca no item de venda do carrinho
+    public void eventAddProduto(View view) {
+        ItemDoCarrinho itemDoCarrinho = new ItemDoCarrinho();
+        // pegar o produto selecionado pelo spinner
+        Produto produtoSelecionado = (Produto) this.spnProdutos.getSelectedItem();
+        // passa os dados do produto da tabela produto
+        Toast.makeText(this, produtoSelecionado.toString() + " - " + produtoSelecionado.getPreco() + " - " + produtoSelecionado.getQuantidadeEmEstoque(), Toast.LENGTH_LONG).show();
+        // pra conseguir pegar a quantidade de cada item:
+        int quantidadeProduto = 0;
+        // senão for digitado nada no formulário:
+        if(this.quantidadeItem.getText().toString().equals("")) {
+            quantidadeProduto = 1;
+        } else{
+            quantidadeProduto = Integer.parseInt(this.quantidadeItem.getText().toString());
+        }
+
+        itemDoCarrinho.setNome(produtoSelecionado.getNome());
+        itemDoCarrinho.setQuantidadeSelecionada(quantidadeProduto);
+        itemDoCarrinho.setPrecoProduto(produtoSelecionado.getPreco());
+        itemDoCarrinho.setprecoDoItemDaVenda(itemDoCarrinho.getPrecoProduto() * itemDoCarrinho.getQuantidadeSelecionada());
+       
+
     }
 }
