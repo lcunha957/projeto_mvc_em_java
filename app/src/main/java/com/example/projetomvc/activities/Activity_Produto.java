21808d06c0a1c1e618d5b818497e1f20ca80440b
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-01 23:08:51 -0300

    aula 07
    .

---
+++ app/src/main/java/com/example/projetomvc/activities/Activity_Produto.java
@@ -0,0 +1,16 @@
+package com.example.projetomvc.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetomvc.R;
+
+public class Activity_Produto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produto);
+    }
+}
