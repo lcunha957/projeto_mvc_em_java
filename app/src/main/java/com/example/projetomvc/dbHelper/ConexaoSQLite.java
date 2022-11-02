bd11195fecfb489ea5691d226ed8c1071b89bfad
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-02 13:08:49 -0300

    aula 28
    .

--- app/src/main/java/com/example/projetomvc/dbHelper/ConexaoSQLite.java
+++ app/src/main/java/com/example/projetomvc/dbHelper/ConexaoSQLite.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class ConexaoSQLite  extends SQLiteOpenHelper {
+public class ConexaoSQLite extends SQLiteOpenHelper {
 
     // deixa claro que essa instância que implementa o Sincroton, instancia 1x no ciclo de vida do sistema.
     private static ConexaoSQLite INSTANCIA_CONEXAO;
@@ -20,29 +20,49 @@ public class ConexaoSQLite  extends SQLiteOpenHelper {
 
     // MÁGICA DO SINCROTON
     public static ConexaoSQLite getInstancia(Context context) {
-      if(INSTANCIA_CONEXAO == null) {
-          INSTANCIA_CONEXAO = new ConexaoSQLite(context);
-      }
-      return INSTANCIA_CONEXAO;
+        if (INSTANCIA_CONEXAO == null) {
+            INSTANCIA_CONEXAO = new ConexaoSQLite(context);
+        }
+        return INSTANCIA_CONEXAO;
     }
 
     // ao criar a aplicação
     @Override
     public void onCreate(SQLiteDatabase db) {
-     // cria tabelas senao existir
+        // cria tabelas senao existir
 
         String sqlTabelaProduto =
                 "CREATE TABLE IF NOT EXISTS produto" +
                         "(" +
-                         "id INTEGER PRIMARY KEY," +
-                         "nome TEXT," +
-                         "quantidade_em_estoque INTEGER," +
-                         "preco REAL" +
-                         ")";
+                        "id INTEGER PRIMARY KEY," +
+                        "nome TEXT," +
+                        "quantidade_em_estoque INTEGER," +
+                        "preco REAL" +
+                        ")";
 
         db.execSQL(sqlTabelaProduto);
 
+        String sqlTabelaVenda =
+                "CREATE TABLE IF NOT EXISTS venda" +
+                        "(" +
+                        "id INTEGER PRIMARY KEY," + // não usaremos autoincrement no sql lite
+                        "data INTEGER" +
+                        ")";
+
+        db.execSQL(sqlTabelaVenda);
+
+        String sqlTabelaItemDaVenda =
+                "CREATE TABLE IF NOT EXISTS item_da_venda" +
+                        "(" +
+                        "id INTEGER PRIMARY KEY," +
+                        "quantidade_vendida INTEGER," +
+                        "id_produto INTEGER," +
+                        "id_venda INTEGER" +
+                        ")";
+
+        db.execSQL(sqlTabelaItemDaVenda);
     }
+    //desinstale o aplicativo pro banco atualizar as tabelas.s
 
     // ao atualizar a versão do banco de dados
     @Override
